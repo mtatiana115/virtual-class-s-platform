43848fc2cfeac9cb1cfd03659d5403a77a3f9006
mtatiana115 <mtatiana115@gmail.com>
2024-07-25 07:07:40 -0500

    fix/Repositories annotations added

--- src/main/java/com/tati/classes_platform/domain/repositories/MultimediaRepository.java
+++ src/main/java/com/tati/classes_platform/domain/repositories/MultimediaRepository.java
@@ -1,9 +1,11 @@
 package com.tati.classes_platform.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tati.classes_platform.domain.entities.Multimedia;
 
+@Repository
 public interface MultimediaRepository extends JpaRepository<Multimedia, Long> {
 
 }
--- src/main/java/com/tati/classes_platform/domain/repositories/StudentRepository.java
+++ src/main/java/com/tati/classes_platform/domain/repositories/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.tati.classes_platform.domain.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.tati.classes_platform.domain.entities.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+  public Page<Student> findAllByIsActiveTrueAndNameContaining(Pageable pageable, String name);
 }
